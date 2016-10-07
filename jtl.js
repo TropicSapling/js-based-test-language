53965ee7b82ae103874c97405d460c834477bc1c
TropicSapling <tropical.productions.scratch@gmail.com>
2016-10-07 19:49:17 +0200

    File reading & added basic example code

--- jtl.js
+++ jtl.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var argv = require('minimist')(process.argv.slice(2));
 
-if(Object.keys(argv).length < 2) {
+if(Object.keys(argv).length < 2 && argv._.length < 1) {
 	console.log("-------- Usage --------");
 	console.log("`node jtl <file> <options>`");
 } else if(argv.h || argv.help) {
@@ -15,4 +15,28 @@ if(Object.keys(argv).length < 2) {
 	console.log("[*] `-v` or `--version`: Display the version number.\n");
 	console.log("[*] `--nogc`: Disable the GC (= Garbage Collector).\n");
 	console.log("NOTE: Currently option `-c` is default because I have not yet found a way to compile directly to machine code or somehow include the C compiler in this compiler.");
+} else {
+	fs.readFile(argv._[0], 'utf8', function(err, data) {
+		if(err) {
+			return console.log(err);
+		}
+		
+		var words = data.split(";");
+		var content;
+		
+		for(i = 0; i < words.length; i++) {
+			words[i] = words[i].split(" ");
+		}
+		
+		var cfile = argv._[0].substring(0, argv._[0].lastIndexOf(".") + 1);
+		
+		// TEMP
+		content = words;
+		
+		fs.writeFile(cfile + "c", content, function(err, data) {
+			if(err) {
+				return console.log(err);
+			}
+		});
+	});
 }
